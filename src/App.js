8f2aaeb17ced80f5d3e9028bd39a69c127473459
Seeyong Lee <seeyong987@gmail.com>
2020-10-08 23:41:29 +0900

    update with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
@@ -6,14 +7,15 @@ class App extends React.Component {
     isLoading: true,
     movies: [],
   };
+  getMovies = async () => {
+    const moives = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are loading"}</div>;
+    return <div>{isLoading ? "Loading..." : "We are loaded"}</div>;
   }
 }
 
